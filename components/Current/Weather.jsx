47dd9f377ea7a43866c91a3dbdb7ef6d60cdf4f8
DaniK <danikordonezgarcia@gmail.com>
2023-08-27 01:00:38 +0200

    add image and basic info

--- components/Current/Weather.jsx
+++ components/Current/Weather.jsx
@@ -1,16 +1,21 @@
+import Image from 'next/image'
+
 export function Weather ({ data }) {
-  const { name } = data.location
+  const { name, country } = data.location
   const { temp_c, feelslike_c, humidity, wind_kph } = data.current
   const { text } = data.current.condition
 
   console.log(data)
 
-
   return (
     <>
-      <span>{name}</span>
-      <p className='text-9xl'>{Math.round(temp_c)}&#176;</p>
-      <p className='uppercase'>{text}</p>
+      <span>{name}, {country}</span>
+      <p className='text-9xl font-bold text-violet-700'>{Math.round(temp_c)}&#176;</p>
+
+      <div className='flex items-center'>
+        <p className='uppercase text-violet-900'>{text}</p>
+        <Image src={`https:${data.current.condition.icon}`} alt={text} height={64} width={64}/>
+      </div>
 
       <section className='flex  gap-12 bg-slate-200 p-5 rounded-full'>
         <div className='flex flex-col items-center'>
