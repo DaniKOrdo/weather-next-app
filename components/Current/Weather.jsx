08bb4766d0e607a22e004c7aaf9477914bab7d42
DaniK <danikordonezgarcia@gmail.com>
2023-08-27 14:12:27 +0200

    changed colors

--- components/Current/Weather.jsx
+++ components/Current/Weather.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export function Weather ({ data }) {
   const { name, country } = data.location
-  const { temp_c, feelslike_c, humidity, wind_kph } = data.current
+  const { temp_c: tempC, feelslike_c: feelslikeC, humidity, wind_kph: windKph } = data.current
   const { text } = data.current.condition
 
   console.log(data)
@@ -10,16 +10,16 @@ export function Weather ({ data }) {
   return (
     <>
       <span>{name}, {country}</span>
-      <p className='text-9xl font-bold text-violet-700'>{Math.round(temp_c)}&#176;</p>
+      <p className='text-9xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-700 to-indigo-500'>{Math.round(tempC)}&#176;</p>
 
       <div className='flex items-center'>
-        <p className='uppercase text-violet-900'>{text}</p>
-        <Image src={`https:${data.current.condition.icon}`} alt={text} height={64} width={64}/>
+        <p className='uppercase text-indigo-900'>{text}</p>
+        <Image src={`https:${data.current.condition.icon}`} alt={text} height={64} width={64} />
       </div>
 
-      <section className='flex  gap-12 bg-slate-200 p-5 rounded-full'>
+      <section className='flex  gap-12 bg-indigo-50 p-5 rounded-full'>
         <div className='flex flex-col items-center'>
-          <span>{Math.round(feelslike_c)}</span>
+          <span>{Math.round(feelslikeC)}</span>
           <span className='text-xs'>Feels like</span>
         </div>
         <div className='flex flex-col items-center'>
@@ -27,7 +27,7 @@ export function Weather ({ data }) {
           <span className='text-xs'>Humidity</span>
         </div>
         <div className='flex flex-col items-center'>
-          <span>{Math.round(wind_kph)}</span>
+          <span>{Math.round(windKph)}</span>
           <span className='text-xs'>Wind speed</span>
         </div>
       </section>
