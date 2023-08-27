1e9b583c4947dc7b29af3f34ab0e88a9e55d20c1
DaniK <danikordonezgarcia@gmail.com>
2023-08-27 14:12:17 +0200

    add icon

--- app/page.js
+++ app/page.js
@@ -1,18 +1,15 @@
 'use client'
 import { useEffect, useState } from 'react'
-// import Image from 'next/image'
-
+import Image from 'next/image'
 import { Weather } from '@/components/Current/Weather'
 import { WeatherChart } from '@/components/Current/WeatherChart'
+import searchIcon from '@/components/icons/searchIcon.svg'
 
 export default function Home () {
   const [city, setCity] = useState('palma')
   const [weather, setWeather] = useState({})
-  const [units, setUnits] = useState('metric')
+  // const [units, setUnits] = useState('metric')
   const [loading, setLoading] = useState(false)
-
-  // https://openweathermap.org/current
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`
 
   // https://rapidapi.com/weatherapi/api/weatherapi-com
   const url = `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${city}&days=3&alerts=yes`
@@ -28,6 +25,7 @@ export default function Home () {
   const fetchWeather = (e) => {
     if (e) e.preventDefault()
     setLoading(true)
+
     fetch(url, options)
       .then((res) => res.json())
       .then((data) => {
@@ -36,9 +34,10 @@ export default function Home () {
       .catch((err) => {
         console.error('ERROR -> ', err)
       })
-
-    setLoading(false)
-    setCity('')
+      .finally(() => {
+        setLoading(false)
+        setCity('')
+      })
   }
 
   useEffect(() => {
@@ -46,47 +45,26 @@ export default function Home () {
   }, [])
 
   return (
-    <main className='flex min-h-screen flex-col items-center p-24'>
+    <main className='flex min-h-screen flex-col items-center md:p-24 p-8 text-indigo-800 lg:max-w-6xl lg:mx-auto'>
       <div className='card flex flex-col items-center w-full p-2'>
-        <form className='flex items-center p-2' onSubmit={fetchWeather}>
-          <input
-            className='text-2xl border rounded-full pl-5 p-2 text-black bg-transparent focus:outline-none placeholder-black'
-            type='text'
-            placeholder='City'
-            onChange={(e) => setCity(e.target.value)}
-          />
-          {/* <div className='flex mt-4'>
-            <label className='mr-2' htmlFor='metric'>
-              <input
-                className='mr-2'
-                type='radio'
-                name='units'
-                id='metric'
-                value='metric'
-                checked={units === 'metric'}
-                onChange={(e) => setUnits(e.target.value)}
-              />
-              Metric
-            </label>
-            <label htmlFor='imperial'>
-              <input
-                className='mr-2'
-                type='radio'
-                name='units'
-                id='imperial'
-                value='imperial'
-                checked={units === 'imperial'}
-                onChange={(e) => setUnits(e.target.value)}
-              />
-              Imperial
-            </label>
+        <form className='flex items-center p-2 w-full md:w-auto'>
+          <div className='relative w-full'>
+            <input
+              className='w-full md:text-2xl text border rounded-full pl-4 p-2 text-indigo-800 bg-transparent focus:outline-none placeholder-indigo-800'
+              type='text'
+              placeholder='City'
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <Image
+              priority
+              src={searchIcon}
+              height={32}
+              width={32}
+              alt='search icon'
+              className='absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer '
+              onClick={fetchWeather}
+            />
           </div>
-          <button
-            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4'
-            type='submit'
-          >
-            Search
-          </button> */}
         </form>
         {loading && <p className='text-2xl'>Loading...</p>}
         {weather.current && <Weather data={weather} />}
