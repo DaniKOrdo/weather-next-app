21985dfc9fc958a531063ec8133bec5d1ca01de4
DaniK <danikordonezgarcia@gmail.com>
2023-08-29 23:11:04 +0200

    fix input r-pading

--- app/page.js
+++ app/page.js
@@ -53,7 +53,7 @@ export default function Home () {
         <form className='flex items-center p-2 w-full md:w-auto' onSubmit={handleSubmit}>
           <div className='relative w-full'>
             <input
-              className='w-full md:text-xl text border rounded-full pl-4 p-2 text-indigo-800 bg-transparent focus:outline-none placeholder-indigo-800 placeholder-opacity-80'
+              className='w-full md:text-xl text border rounded-full pl-4 p-2 pr-12 text-indigo-800 bg-transparent focus:outline-none placeholder-indigo-800 placeholder-opacity-80'
               type='text'
               placeholder='City'
               onChange={(e) => setCity(e.target.value)}
