62e41f1b7e9533549a750e26897800a015923662
DaniK <danikordonezgarcia@gmail.com>
2023-09-02 11:47:41 +0200

    add current position and handle errors

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import { Weather } from '@/components/Current/Weather'
 import { WeatherChart } from '@/components/Current/WeatherChart'
@@ -7,56 +7,87 @@ import searchIcon from '@/components/icons/searchIcon.svg'
 import sadCloud from '@/components/icons/sadCloud.svg'
 
 export default function Home () {
+  const cities = ['Mallorca', 'Japan', 'London', 'Buenos Aires']
   const [city, setCity] = useState('')
   const [data, setData] = useState({})
-  // const [units, setUnits] = useState('metric')
   const [loading, setLoading] = useState(false)
+  const [noDataAvailable, setNoDataAvailable] = useState(false)
 
-  // https://rapidapi.com/weatherapi/api/weatherapi-com
-  const url = `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${city}&days=3&alerts=yes`
-
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
-      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-    }
-  }
-
-  const fetchWeather = () => {
-    if (!city.trim()) return
+  const fetchWeather = (queryCity) => {
+    if (!queryCity) return
     setLoading(true)
+
+    const url = `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${queryCity}&days=3&alerts=yes`
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
+        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+      }
+    }
 
     fetch(url, options)
       .then((res) => res.json())
       .then((data) => {
         setData(data)
-        console.log(data)
+        setNoDataAvailable(!data.forecast || data.forecast.forecastday.length === 0)
       })
       .catch((err) => {
         console.error('ERROR -> ', err)
+        setNoDataAvailable(true)
       })
       .finally(() => {
         setLoading(false)
-        setCity('')
       })
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    fetchWeather()
+  const handleSearch = () => {
+    fetchWeather(city)
   }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault() // Evitar enviar el formulario por defecto
+      fetchWeather(city)
+    }
+  }
+
+  const handleGeolocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        const latitude = position.coords.latitude
+        const longitude = position.coords.longitude
+        setCity(`${latitude},${longitude}`)
+        fetchWeather(`${latitude},${longitude}`)
+      },
+      function (error) {
+        console.error('Geolocation error:', error)
+        setCity(getRandomCity())
+        fetchWeather(getRandomCity())
+      }
+    )
+  }
+
+  const getRandomCity = () => {
+    return cities[Math.floor(Math.random() * cities.length)]
+  }
+
+  useEffect(() => {
+    handleGeolocation()
+  }, [])
 
   return (
     <main className='flex min-h-screen flex-col items-center md:p-24 p-8 text-indigo-800 lg:max-w-6xl lg:mx-auto'>
       <div className='card flex flex-col items-center w-full p-2'>
-        <form className='flex items-center p-2 w-full md:w-auto' onSubmit={handleSubmit}>
+        <form className='flex items-center p-2 w-full md:w-auto'>
           <div className='relative w-full'>
             <input
               className='w-full md:text-xl text border rounded-full pl-4 p-2 pr-12 text-indigo-800 bg-transparent focus:outline-none placeholder-indigo-800 placeholder-opacity-80'
               type='text'
               placeholder='City'
+              // value={city}
               onChange={(e) => setCity(e.target.value)}
+              onKeyDown={handleKeyDown}
               autoFocus
             />
             <Image
@@ -66,14 +97,19 @@ export default function Home () {
               width={32}
               alt='search icon'
               className='absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer'
-              onClick={fetchWeather}
+              onClick={handleSearch}
             />
           </div>
         </form>
         {!data.current && loading && <p className='text-2xl'>Loading...</p>}
         {data.current && <Weather data={data} />}
-        {data.forecast && <WeatherChart data={data.forecast.forecastday[0].hour} localTime={data.location.localtime} />}
-        {data.error &&
+        {noDataAvailable && !loading && (
+          <p className='text-2xl'>No forecast data available.</p>
+        )}
+        {data.forecast && data.forecast.forecastday.length > 0 && (
+          <WeatherChart data={data.forecast.forecastday[0].hour} localTime={data.location.localtime} />
+        )}
+        {data.error && (
           <section className='flex flex-col items-center'>
             <Image
               priority
@@ -81,12 +117,11 @@ export default function Home () {
               height={256}
               width={256}
               alt='sad cloud'
-              onClick={fetchWeather}
+              onClick={handleSearch}
             />
-            <p className='text-2xl'>
-              {data.error.message}
-            </p>
-          </section>}
+            <p className='text-2xl'>{data.error.message}</p>
+          </section>
+        )}
       </div>
     </main>
   )
