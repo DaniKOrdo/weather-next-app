fa16a55c1f9f9040e809769a08f81f836d8a5cf1
DaniK <danikordonezgarcia@gmail.com>
2023-09-02 15:29:13 +0200

    add input autocomplete

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,10 @@ export const metadata = {
 export default function RootLayout ({ children }) {
   return (
     <html lang='en'>
+      <script
+        async
+        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_KEY}&language=en&components=locality&libraries=places&callback=myCallbackFunc`}
+      />
       <body className={rubik.className}>{children}</body>
     </html>
   )
--- components/InputForm.jsx
+++ components/InputForm.jsx
@@ -1,19 +1,53 @@
 import Image from 'next/image'
+import { useState } from 'react'
 import searchIcon from '@/components/icons/searchIcon.svg'
+import PlacesAutocomplete from 'react-places-autocomplete'
 
-export default function InputForm ({ city, setCity, handleSearch, handleKeyDown }) {
+export default function InputForm ({ setCity, handleSearch, fetchWeather }) {
+  const [input, setInput] = useState('')
+
   return (
     <form className='flex items-center p-2 w-full md:w-auto' onSubmit={handleSearch}>
       <div className='relative w-full'>
-        <input
-          className='w-full md:text-xl text border rounded-full pl-4 p-2 pr-12 text-indigo-800 bg-transparent focus:outline-none placeholder-indigo-800 placeholder-opacity-80'
-          type='text'
-          placeholder='Search for a city...'
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          onKeyDown={handleKeyDown}
-          autoFocus
-        />
+        <PlacesAutocomplete
+          value={input}
+          onChange={setInput}
+          onSelect={(selectedCity) => {
+            console.log(selectedCity)
+            setCity(selectedCity)
+            fetchWeather(selectedCity)
+          }}
+          googleCallbackName='initOne'
+        >
+          {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+            <div className='relative w-full'>
+              <input
+                {...getInputProps({
+                  placeholder: 'Search for a city...',
+                  className: 'w-full md:text-xl text border rounded-full pl-4 p-2 pr-12 text-indigo-800 bg-transparent focus:outline-none placeholder-indigo-800 placeholder-opacity-80',
+                  type: 'text',
+                  autoFocus: true
+                })}
+              />
+              <div className='absolute z-10 w-full bg-white rounded-b-lg border-t-0'>
+                {loading && ''}
+                {suggestions.map((suggestion) => {
+                  const style = {
+                    backgroundColor: suggestion.active ? '#ebebeb' : '#fff',
+                    cursor: 'pointer',
+                    padding: '0.5rem'
+                  }
+                  return (
+                    <div key={suggestion.placeId} className='rounded-b-lg' {...getSuggestionItemProps(suggestion, { style })}>
+                      {suggestion.description}
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+          )}
+        </PlacesAutocomplete>
+
         <Image
           priority
           src={searchIcon}
