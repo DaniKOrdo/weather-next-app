4bc82b5728e6dba143dc80a549898cbf77cbeb1a
DaniK <danikordonezgarcia@gmail.com>
2023-09-12 15:37:23 +0200

    add Head component with og:image

---
+++ app/Head.jsx
@@ -0,0 +1,18 @@
+export function Head () {
+  return (
+    <head>
+      <meta property='og:url' content='https://weather-next-app.danik.dev'>
+      <meta property='og:type' content='website'>
+      <meta property='og:title' content='Weather Next App'>
+      <meta property='og:description' content='Weather Next App'>
+      <meta property='og:image' content='https://weather-next-app.danik.dev/weather-next-app.png'>
+
+      <meta name='twitter:card' content='summary_large_image'>
+      <meta property='twitter:domain' content='weather-next-app.danik.dev'>
+      <meta property='twitter:url' content='https://weather-next-app.danik.dev'>
+      <meta name='twitter:title' content='Weather Next App'>
+      <meta name='twitter:description' content='Weather Next App'>
+      <meta name='twitter:image' content='https://weather-next-app.danik.dev/weather-next-app.png'>
+    </head>
+  )
+}
